8b878cefeefa64f0b2e0f4e0dbb0f650bc830c46
Arthur <arthurrapach06@gmail.com>
2022-05-02 19:16:48 -0300

    3 Condições (if, else if e else)

---
+++ condicoes/src/condicoes/main.java
@@ -0,0 +1,26 @@
+package condicoes;
+
+import javax.swing.JOptionPane;
+
+public class main {
+
+	public static void main(String[] args) {
+			
+	//variables
+	int age;
+	
+	JOptionPane.showMessageDialog(null, "Seja bem vindo ao nosso programa!");
+	age = Integer.parseInt(JOptionPane.showInputDialog(null, "Informe sua Idade"));
+	
+	if(age<18) {
+		JOptionPane.showMessageDialog(null, "Você ainda é menor de idade!");
+	}
+	else if(age==18) {
+		JOptionPane.showMessageDialog(null, "Parabéns, você acabou de completar maior idade!");
+	}else {
+		JOptionPane.showMessageDialog(null, "ta ficando velho!");
+	}
+
+	}
+
+}
--- segunda_aula/src/aula2/main.java
+++ segunda_aula/src/aula2/main.java
@@ -1,10 +1,24 @@
 package aula2;
 
+import javax.swing.JOptionPane;
+
 public class main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		// first contact with JOPTIONPANE
+		
+		String name, city, adress; 
+		int cep=0, tel=0;
+		
+		JOptionPane.showMessageDialog(null, "Bem Vindo a nossa Agenda do Senac");
+		name = JOptionPane.showInputDialog("Informe seu nome:\n");
+		tel = Integer.parseInt(JOptionPane.showInputDialog("Informe seu telefone:\n"));
+		adress = JOptionPane.showInputDialog("Informe seu endereço:\n");
+		city = JOptionPane.showInputDialog("Informe sua cidade:\n");
+		cep = Integer.parseInt (JOptionPane.showInputDialog("Informe seu CEP:\n"));
+		
+		JOptionPane.showMessageDialog(null, "Os dados que inseriu foram:\nNome: " + name + "Telefone: " + tel);
+		JOptionPane.showMessageDialog(null, "\nEndereço: " + adress + "\nCidade: " + city + "\nCEP: " + cep);
 	}
 
 }
