8e6ab2063dce23957f0d94b3d50511e76d7fa3ab
Arthur <arthurrapach06@gmail.com>
2022-05-02 14:31:19 -0300

    aula das condicoes

--- segunda_aula/src/aula2/main.java
+++ segunda_aula/src/aula2/main.java
@@ -1,10 +1,24 @@
 package aula2;
 
+import javax.swing.JOptionPane;
+
 public class main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	
+	int age;
+	
+	JOptionPane.showInternalMessageDialog(null,"Bem Vindo ao Calculo IMC");
+	age = JOptionPane.showMessageDialog(null, "Informe sua Idade: ");
+	
+	if(age<18) {
+		JOptionPane.showInternalMessageDialog(null, "Você ainda é menor de idade");
 	}
-
+	else if(age>=18 && age<=35) {
+		JOptionPane.showInternalMessageDialog(null, "Parabens voce eh maior de idade");
+	}
+	else{
+		JOptionPane.showInternalMessageDialog(null, "Voce ja esta velhinho");
+	}
+	}
 }
