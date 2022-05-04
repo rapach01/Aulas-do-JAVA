dbf5b6ae4b0d05153aec4421f2b97d0015f598ec
Arthur <arthurrapach06@gmail.com>
2022-05-04 20:27:53 -0300

    Correção

--- condicoes/src/condicoes/main.java
+++ condicoes/src/condicoes/main.java
@@ -6,21 +6,22 @@ public class main {
 
 	public static void main(String[] args) {
 			
-	//variables
-	int age;
+		//variables
+		int age;
 	
-	JOptionPane.showMessageDialog(null, "Seja bem vindo ao nosso programa!");
-	age = Integer.parseInt(JOptionPane.showInputDialog(null, "Informe sua Idade"));
+		//user's info
+		JOptionPane.showMessageDialog(null, "Seja bem vindo ao nosso programa!");
+		age = Integer.parseInt(JOptionPane.showInputDialog(null, "Informe sua Idade"));
 	
-	if(age<18) {
-		JOptionPane.showMessageDialog(null, "Você ainda é menor de idade!");
-	}
-	else if(age==18) {
-		JOptionPane.showMessageDialog(null, "Parabéns, você acabou de completar maior idade!");
-	}else {
-		JOptionPane.showMessageDialog(null, "ta ficando velho!");
-	}
-
+		//conditions
+		if(age<18) {
+			JOptionPane.showMessageDialog(null, "Você ainda é menor de idade!");
+		}
+		else if(age==18) {
+			JOptionPane.showMessageDialog(null, "Parabéns, você acabou de completar maior idade!");
+		}else {
+			JOptionPane.showMessageDialog(null, "ta ficando velho!");
+		}
 	}
 
 }
