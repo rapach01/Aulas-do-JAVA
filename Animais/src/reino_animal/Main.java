6812aa992d2d6c540d01bd5ef6408018d10c9c39
Arthur <arthurrapach06@gmail.com>
2022-05-12 19:14:03 -0300

    Animais

--- Animais/src/reino_animal/Main.java
+++ Animais/src/reino_animal/Main.java
@@ -1,33 +1,13 @@
 package reino_animal;
 
-import javax.swing.JOptionPane;
-
 public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		Animal c = new Cachorro();
+		TesteAnimal ta = new TesteAnimal();
 		
-		JOptionPane.showMessageDialog(null, c.getNameDog());
-		JOptionPane.showMessageDialog(null, c.getDogAge());
-		JOptionPane.showMessageDialog(null, c.getDogNoise());
+		ta.teste();
 		
-		Animal c2 = new Cavalo();
-		
-		JOptionPane.showMessageDialog(null, c2.getNameHorse());
-		JOptionPane.showMessageDialog(null, c2.getHorseAge());
-		JOptionPane.showMessageDialog(null, c2.getHorseNoise());
-		
-		Animal c3 = new Preguiça();
-		
-		JOptionPane.showMessageDialog(null, c3.getPreguicaName());
-		JOptionPane.showMessageDialog(null, c3.getPreguicaNoise());
-		JOptionPane.showMessageDialog(null, c3.getPreguicaAge());
-
-
-
-
-
 
 	}
 
--- Animais/src/reino_animal/TesteAnimal.java
+++ Animais/src/reino_animal/TesteAnimal.java
@@ -1,15 +1,27 @@
 package reino_animal;
 
+import javax.swing.JOptionPane;
+
 public class TesteAnimal extends Animal{
-	Cachorro c = new Cachorro();
-	Cavalo ca = new Cavalo();
-	Preguiça p = new Preguiça();
-	
+	int op, i;;
 	String[] Animals = new String[10];
 	
 	void teste() {
-		for()
-		Animals[1] = c.Dog();
+		for( i=0; i<10; i++) {
+			op= Integer.parseInt(JOptionPane.showInputDialog(null, "Qual você deseja ouvir primeiro?\n(1)Cachorro\n(2)Cavalo\n(3)Preguiça"));
+			
+			if(op==1) {
+				Animals[i] = getDogNoise();
+			}else if (op==2) {
+				Animals[i] = getHorseNoise();
+			}else if (op==3) {
+				Animals[i] = getPreguicaNoise();
+			}
+		}
+		for( i=0; i<10; i++) {
+			JOptionPane.showMessageDialog(null, Animals[i]);
+		}
 	}
+		
 	
 }
